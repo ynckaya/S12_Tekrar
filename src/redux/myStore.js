333bb7c85bea5fa9b934b4090b965db544382802
ynckaya <ynckaya@gmail.com>
2025-12-16 14:58:10 +0300

    Redux logger added

--- src/redux/myStore.js
+++ src/redux/myStore.js
@@ -1,11 +1,12 @@
-import { legacy_createStore as createStore, combineReducers} from 'redux';
+import { legacy_createStore as createStore, combineReducers, applyMiddleware} from 'redux';
 import { taskReducer } from './taskReducer';
 import { userReducer } from './userReducer';
+import logger from 'redux-logger';
 
 const allMyReducer = combineReducers({
     taskR: taskReducer,
     userR: userReducer
 });
 
-export const myStore = createStore(allMyReducer);
+export const myStore = createStore(allMyReducer, applyMiddleware(logger));
 
